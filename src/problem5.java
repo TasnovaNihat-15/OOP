f106b4fc3ee9241e4ca548511d345a6b1d850f35
TasnovaNihat-15 <ramisakhan418@gmail.com>
2025-01-07 11:13:27 +0600

    ramisa

--- src/problem5.java
+++ src/problem5.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class problem5 {
     public static void main(String[] args) {
-        double a = 3;
+        double arm = 3;
 
-        double area = (Math.sqrt(3) / 4) * Math.pow(a, 2);
+        double area = (Math.sqrt(3) / 4) * Math.pow(arm, 2);
 
         System.out.println(area);
     }
